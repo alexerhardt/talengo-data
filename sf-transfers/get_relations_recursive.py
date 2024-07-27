711f6e0bebd03558d286ac9181cdc3b84bb39b7f
alexerhardt <alex.erhardt.es@gmail.com>
2024-07-27 18:06:38 +0200

    Add exclusions logic

--- sf-transfers/get_relations_recursive.py
+++ sf-transfers/get_relations_recursive.py
@@ -9,12 +9,19 @@ sf_prod = get_sf_prod()
 MAX_DEPTH = 20
 starting_object = "compensation__c"
 
+# Read from data/model.json using path relative to this file
+path_in = os.path.join(os.path.dirname(__file__), "data/model.json")
+with open(path_in) as f:
+    object_model = json.load(f)
 
-def get_relations_recursive(object_name, rels_map, depth=0):
+GLOBAL_EXCLUSIONS = ["User", "Group"]
+
+
+def get_relations_recursive(object_name, rels_map, depth=0, exclude=True):
     if depth > MAX_DEPTH:
         raise RecursionError("Max depth reached")
 
-    if object_name in rels_map:
+    if object_name in rels_map or object_name in GLOBAL_EXCLUSIONS:
         return rels_map
 
     rels_map = copy.deepcopy(rels_map)
@@ -25,6 +32,10 @@ def get_relations_recursive(object_name, rels_map, depth=0):
         for field in fields_metadata["fields"]
         if field["createable"] and field["type"] == "reference"
     ]
+
+    if exclude:
+        exclusions = object_model[object_name]["exclusions"]
+        fields = [field for field in fields if field[0] not in exclusions]
 
     for field in fields:
         rels_map.setdefault(object_name, {})[field[0]] = field[1]
