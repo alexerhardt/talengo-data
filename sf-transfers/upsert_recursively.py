751f328412754f52b5fd0eb4638f9aabaa713bb6
alexerhardt <alex.erhardt.es@gmail.com>
2024-08-12 12:03:15 +0200

    Fix upstream reference insertion

--- sf-transfers/upsert_recursively.py
+++ sf-transfers/upsert_recursively.py
@@ -1,17 +1,7 @@
-# TODO: Add logging with numbers of calls, etc
-# Add info on id and object being inserted
 from config import PRODUCTION_ID_KEY
 import copy
 
 GLOBAL_EXCLUSIONS = ["User", "Group", "RecordType"]
-
-
-def update_metadata_map(sf, object_key, metadata_map):
-    if object_key not in metadata_map:
-        new_map = copy.deepcopy(metadata_map)
-        new_map[object_key] = sf.__getattr__(object_key).describe()
-        return new_map
-    return metadata_map
 
 
 def upsert_record_and_references(
@@ -22,52 +12,107 @@ def upsert_record_and_references(
 
     :param source: simple_salesforce.Salesforce source instance
     :param target: simple_salesforce.Salesforce target instance
-    :param object_key: str
-    :param object_id: str
+    :param object_key: str Name of the object in Salesforce (ex: "Contact")
+    :param object_id: str Salesforce Id of the source object to insert
     :param metadata_map: dict
+    :param object_model_map: dict
     :return: None
     """
+    print(f"Upserting {object_key} {object_id}")
+
     # Updates our metadata definitions if we don't have them
-    fields_metadata = update_metadata_map(source, object_key, metadata_map)
+    updated_metadata_map = update_metadata_map(source, object_key, metadata_map)
+    object_fields_metadata = updated_metadata_map[object_key]["fields"]
 
-    # Get all the object data we want to migrate, minus non-creatable and exclusions
-    exclusions = object_model_map["object_key"].get("exclusions", [])
-    fields = [
-        field["name"]
-        for field in fields_metadata["fields"]
-        if field["createable"] and field["name"] not in exclusions
-    ]
+    exclusions = object_model_map[object_key].get("exclusions", [])
+    fields_with_refs = get_fields_with_refs(exclusions, object_fields_metadata)
+
+    fields = [field[0] for field in fields_with_refs]
     query = f"SELECT {', '.join(fields)} FROM {object_key} WHERE Id = '{object_id}'"
-    record = source.query_all(query)["records"][0]
-
-    # The only references with more than one referenceTo are User and Group; excluded
-    refs = [
-        (field["name"], field["referenceTo"][0])
-        for field in fields_metadata["fields"]
-        if field["type"] == "reference"
-        and field["referenceTo"][0] != "User"
-        and field["referenceTo"][0] != "Group"
-        and field["referenceTo"][0] != "RecordType"
-    ]
+    record = source.query(query)["records"][0]
 
     # Before inserting into target, depth-first insert all references
+    refs = get_object_refs_to_upsert(fields_with_refs)
+    parent_refs = {}
     for ref in refs:
-        ref_object_key = ref[1]
-        ref_object_id = record.get(ref[0])
-        fields_metadata = upsert_record_and_references(
-            source, target, ref_object_key, ref_object_id, object_model_map
-        )
+        ref_key, ref_object_key = ref[0], ref[1]
+        ref_object_id = record[ref[0]]
+        if ref_object_id:
+            inserted_id, updated_metadata_map = upsert_record_and_references(
+                source,
+                target,
+                ref_object_key,
+                ref_object_id,
+                updated_metadata_map,
+                object_model_map,
+            )
+            parent_refs[ref_key] = inserted_id
 
-    record_data = {field: record.get(field) for field in fields}
-    record_data[PRODUCTION_ID_KEY] = record["Id"]
+    # If the reference has been upserted previously, we replace the value with the Id
+    record_data = {
+        field: parent_refs[field] if field in parent_refs else record.get(field)
+        for field in fields
+    }
 
     try:
+        # TODO: Improve __getattr__, get the function with a function
         target.__getattr__(object_key).upsert(
-            f"${PRODUCTION_ID_KEY}/{object_id}", record_data
+            f"{PRODUCTION_ID_KEY}/{object_id}", record_data
         )
-        print(f"Upserted {object_key}: {record.get('Name', '')}")
+        inserted_id = target.__getattr__(object_key).get_by_custom_id(
+            PRODUCTION_ID_KEY, object_id
+        )["Id"]
+        print(f"Upserted {object_key} {object_id} {record.get('Name', '')}")
     except Exception as e:
-        print(f"Error upserting {object_key} {record.get('Name', '')}: {e}")
+        print(f"Upsert error {object_key} {object_id} {record.get('Name', '')}: {e}")
+        inserted_id = None
 
     # We return the fields metadata to avoid having to re-query it
-    return fields_metadata
+    return inserted_id, updated_metadata_map
+
+
+def update_metadata_map(sf, object_key, metadata_map):
+    """
+    Updates the metadata map with the metadata for the object_key if not present.
+    :param sf: Simple Salesforce instance
+    :param object_key: Name of the object in Salesforce (ex: "Contact")
+    :param metadata_map: The existing metadata map
+    :return:
+    """
+    if object_key not in metadata_map:
+        new_map = copy.deepcopy(metadata_map)
+        new_map[object_key] = sf.__getattr__(object_key).describe()
+        return new_map
+    return metadata_map
+
+
+def get_fields_with_refs(exclusions, object_fields_metadata) -> list[tuple[str, str]]:
+    """
+    Returns a list of fields that have references to other objects.
+    :param exclusions:
+    :param object_fields_metadata:
+    :return: List of tuples with the field name and the reference object name
+    """
+    return [
+        (field["name"], field["referenceTo"])
+        for field in object_fields_metadata
+        if field["createable"] and field["name"] not in exclusions
+    ]
+
+
+def get_object_refs_to_upsert(fields_with_references) -> list[tuple[str, str]]:
+    """
+    Further filters the fields with references to exclude system objects.
+    :param fields_with_references:
+    :return: List of tuples with the field name and the reference object name
+    """
+    return [
+        # We pick the field name and the first reference object;
+        # having filtered out User and Group, there shouldn't be more than one reference
+        (field[0], field[1][0])
+        for field in fields_with_references
+        if len(field[1]) > 0
+        and field[1][0] != "User"
+        and field[1][0] != "Group"
+        and field[1][0] != "RecordType"
+    ]
