7b95bc7c3ff422ac298fa76515dace89898dbef4
alexerhardt <alex.erhardt.es@gmail.com>
2024-09-17 18:18:22 +0200

    Add invoice copying script

---
+++ sf-transfers/invoice.py
@@ -0,0 +1,10 @@
+from config import get_sf_prod, get_sf_sandbox, get_object_model
+from migrate import migrate
+
+sf_prod = get_sf_prod()
+sf_sandbox = get_sf_sandbox()
+object_model = get_object_model()
+
+query = "SELECT Id FROM Facturas__c WHERE Anno_Factura_Reporte__c = 2024"
+
+migrate(sf_prod, sf_sandbox, "Facturas__c", object_model, query)
